445de88a72db5df34cc917f8906f48c770a536db
Joshua <joshuacomfort@gmail.com>
2022-11-18 11:35:54 -0500

    Removed arrow button from main menu

--- app/src/main/java/com/example/puzzlesolver/MainMenu.java
+++ app/src/main/java/com/example/puzzlesolver/MainMenu.java
@@ -1,11 +1,17 @@
 package com.example.puzzlesolver;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+    //Private data members corresponding to the buttons on screen
+    private Button converter_button;
+    private Button word_button;
+    private Button lights_out_button;
+    private Button slider_button;
 
+    //On menu creation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
