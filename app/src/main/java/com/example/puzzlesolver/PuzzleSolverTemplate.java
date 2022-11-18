4bfe527375fbbbfe9ca1931519b8fa8e0c650b6c
Joshua <joshuacomfort@gmail.com>
2022-11-18 11:45:58 -0500

    Started implementation of puzzle activity abstract class

---
+++ app/src/main/java/com/example/puzzlesolver/PuzzleSolverTemplate.java
@@ -0,0 +1,34 @@
+package com.example.puzzlesolver;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class PuzzleSolverTemplate extends AppCompatActivity {
+    private Puzzle problem;
+
+    //Find solution to the puzzle
+    public final void solveProblem()
+    {
+        //Read problem in from the view
+        readProblem();
+
+        //If the puzzle has a solution display it, otherwise output an error
+        if (notSolvable())
+            outputError();
+        else
+            outputSolution();
+    }
+
+    //Abstract functions to be implemented in subclasses
+    public abstract void readProblem();
+    public abstract void outputError();
+    public abstract void outputSolution();
+
+    //Check if the puzzle has a solution
+    public boolean notSolvable()
+    {
+        return !puzzle.isSolvable();
+    }
+}
